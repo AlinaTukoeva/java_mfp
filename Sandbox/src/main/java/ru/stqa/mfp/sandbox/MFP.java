3dda10804b37a49c2d2112299f7cacd22616150b
AlinaTukoeva <49677884+AlinaTukoeva@users.noreply.github.com>
2019-04-22 14:26:37 +0700

    добавлено несколько функций

--- Sandbox/src/main/java/ru/stqa/mfp/sandbox/MFP.java
+++ Sandbox/src/main/java/ru/stqa/mfp/sandbox/MFP.java
@@ -2,6 +2,23 @@ package ru.stqa.mfp.sandbox;
 
 public class MFP {
 	public static void main(String[] args){
-		System.out.println("Hello, world");
+		hello("world");
+		double l = 5;
+		double a = 4;
+		double b = 6;
+		System.out.println(area2(a, b));
+		System.out.println(area(l));
+	}
+
+	public static void hello(String s) {
+		System.out.println("Hello, " + s);
+	}
+
+	public static double area (double l){
+		return l * l;
+	}
+
+	public static double area2 (double a, double b){
+		return a * b;
 	}
 }
